2c99bca6612d9a57c91a5a4354c3518a0d1a1cf4
RuntimeState <runtimestate@gmail.com>
2012-11-14 15:39:53 +0800

    Signed-off-by: RuntimeState <runtimestate@gmail.com>

--- src/com/life/android/ringmanager/RingReceiver.java
+++ src/com/life/android/ringmanager/RingReceiver.java
@@ -9,7 +9,7 @@ public class RingReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Intent ringServiceIntent = new Intent(context, RingService.class);
-
+		ringServiceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
 		context.startService(ringServiceIntent);
 	}
 }
